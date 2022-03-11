e3798bb80c2c70bc21a9d109c457dbe2b23b545d
Theo Lincoln <xantos117@gmail.com>
2022-03-10 23:23:18 -0500

    Some debugging changes. Pigment flow for red seems to work somewhat. Flow layer is the current issue. Do I need 2 shader passes?

--- src/client/client.ts
+++ src/client/client.ts
@@ -8,7 +8,6 @@ let mouseDown = false;
 let mouse = new THREE.Vector3(0,0,1);
 let canvasWidth = window.innerWidth;
 let canvasHeight = window.innerHeight;
-//let blankTextureData = new Uint8Array(canvasWidth * canvasHeight);
 let blankTextureData = new Float32Array(4*canvasWidth*canvasHeight);
 blankTextureData.forEach(() => Math.random());
 let initTex = new THREE.DataTexture(blankTextureData,canvasWidth,canvasHeight,THREE.RGBAFormat,THREE.FloatType);
@@ -16,22 +15,27 @@ initTex.internalFormat = 'RGBA32F';
 initTex.magFilter = THREE.NearestFilter;
 initTex.needsUpdate = true;
 let initTex2 = new THREE.Texture().copy(initTex);
+let isInked = false;
 
 let scene = new THREE.Scene();
 
 let camera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, -10000, 10000);
 camera.position.z = 100;
-let renderer = new THREE.WebGLRenderer({ preserveDrawingBuffer: true });
+let renderer = new THREE.WebGLRenderer({ preserveDrawingBuffer: true, antialias: true });
 renderer.setSize(canvasWidth,canvasHeight);
-document.body.append(renderer.domElement);
+renderer.setScissorTest( true );
+//document.body.append(renderer.domElement);
+
+const container = document.querySelector( '.container' ) as HTMLElement;
+container.appendChild( renderer.domElement );
+
+let sceneL = new THREE.Scene();
+sceneL.background = new THREE.Color( 0xBCD48F );
+
+// sceneR = new THREE.Scene();
+// sceneR.background = new THREE.Color( 0x8FBCD4 );
 
 let bufferScene = new THREE.Scene();
-// let StreamPlusScene = new THREE.Scene();
-// let StreamXScene = new THREE.Scene();
-// let VelDensScene = new THREE.Scene();
-// let RelaxPlusScene = new THREE.Scene();
-// let RelaxXScene = new THREE.Scene();
-
 
 let bufferTexture = new THREE.WebGLRenderTarget( 1, 1, { minFilter: THREE.LinearFilter, magFilter: THREE.NearestFilter});
 
@@ -63,6 +67,18 @@ let OtherVecs1 = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight,
      format: THREE.RGBAFormat,
      type: THREE.FloatType});
 
+let waterAndBoundary1 = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight,
+    {minFilter: THREE.LinearFilter,
+     magFilter: THREE.LinearFilter,
+     format: THREE.RGBAFormat,
+     type: THREE.FloatType});
+
+let redLayer1 = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight,
+    {minFilter: THREE.LinearFilter,
+     magFilter: THREE.LinearFilter,
+     format: THREE.RGBAFormat,
+     type: THREE.FloatType});
+
 let StVecs2 = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight,
     {minFilter: THREE.LinearFilter,
      magFilter: THREE.LinearFilter,
@@ -77,17 +93,18 @@ let OtherVecs2 = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight,
     {minFilter: THREE.LinearFilter,
      magFilter: THREE.LinearFilter,
      format: THREE.RGBAFormat,
-     type: THREE.FloatType});
+     type: THREE.FloatType}); 
 
-//StVecs1.texture.internalFormat = 'RGBA32F';
-//console.log(initTex);
-//console.log(StVecs1.texture);
-//renderer.copyTextureToTexture(new THREE.Vector2(0,0),initTex,StVecs1.texture);
-// renderer.copyTextureToTexture(new THREE.Vector2(0,0),initTex,DiagVecs1.texture);
-// renderer.copyTextureToTexture(new THREE.Vector2(0,0),initTex,OtherVecs1.texture);
-// renderer.copyTextureToTexture(new THREE.Vector2(0,0),initTex,StVecs2.texture);
-// renderer.copyTextureToTexture(new THREE.Vector2(0,0),initTex,DiagVecs2.texture);
-// renderer.copyTextureToTexture(new THREE.Vector2(0,0),initTex,OtherVecs2.texture);   
+let waterAndBoundary2 = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight,
+    {minFilter: THREE.LinearFilter,
+     magFilter: THREE.LinearFilter,
+     format: THREE.RGBAFormat,
+     type: THREE.FloatType});
+let redLayer2 = new THREE.WebGLRenderTarget(canvasWidth, canvasHeight,
+    {minFilter: THREE.LinearFilter,
+     magFilter: THREE.LinearFilter,
+     format: THREE.RGBAFormat,
+     type: THREE.FloatType});
 
 console.log(StVecs1.texture.image);
 
@@ -105,11 +122,6 @@ const paperMat = new THREE.MeshBasicMaterial({
 let vertShader : HTMLElement | null = document.getElementById('vertex-shader');
 let fragShader : HTMLElement | null = document.getElementById('fragment-shader');
 let fragScreen : HTMLElement | null = document.getElementById('fragment-screen');
-// let fragStreamPlus : HTMLElement | null = document.getElementById('streamingPlus');
-// let fragStreamX : HTMLElement | null = document.getElementById('streamingX');
-// let fragVelDensUpd : HTMLElement | null = document.getElementById('velDensUpdate');
-// let fragRelaxPlus : HTMLElement | null = document.getElementById('relaxPlus');
-// let fragRelaxX : HTMLElement | null = document.getElementById('relaxX');
 
 const loader = new THREE.TextureLoader();
 const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/bayer.png');
@@ -133,12 +145,15 @@ let uniforms = {
     stVecs: {type: "t", value: initTex as THREE.Texture},
     diagVecs: {type: "t", value: initTex as THREE.Texture},
     otherVecs: {type: "t", value: initTex as THREE.Texture},
+    reboundTexture: {type: "t", value: initTex as THREE.Texture},
+    pigmentTexture1: {type: "t", value: initTex as THREE.Texture},
     paperTexture: {type: "t", value: PaperTexture},
     shaderStage: {value: 0},
     mDown: {type: "b", value: false}, 
     omg: {value: 0.9},
     rho0: {value: 1.0},
     c: {value: 1.0},
+    ink: {value: false},
 };
 
 
@@ -147,31 +162,84 @@ const sizes = {
     height: canvasHeight
 };
 
-let onMouseMove = function(e: MouseEvent)
-{
-    mouseX = e.pageX
-    mouseY = e.pageY
+let sliderPos = window.innerWidth / 2;
+let slider = document.querySelector( '.slider' ) as HTMLElement;
+function initSlider() {
 
-    if(mouseDown)
-        uniforms.iResolution.value.x = mouseX/canvasWidth;
-        uniforms.iResolution.value.y = 1-mouseY/canvasHeight;
+
+    function onPointerDown(e: PointerEvent) {
+
+        if ( e.isPrimary === false ) return;
+
+        //controls.enabled = false;
+
+        window.addEventListener( 'pointermove', onPointerMove );
+        window.addEventListener( 'pointerup', onPointerUp );
+
+    }
+
+    function onPointerUp(e: PointerEvent) {
+
+        //controls.enabled = true;
+
+        window.removeEventListener( 'pointermove', onPointerMove );
+        window.removeEventListener( 'pointerup', onPointerUp );
+
+    }
+
+    function onPointerMove( e: PointerEvent ) {
+
+        if ( e.isPrimary === false ) return;
+
+        sliderPos = Math.max( 0, Math.min( window.innerWidth, e.pageX ) );
+
+        if(slider){
+            slider.style.left = sliderPos - ( slider.offsetWidth / 2 ) + "px";
+        }
+
+    }
+
+    if(slider){
+        slider.style.touchAction = 'none'; // disable touch scroll
+        slider.addEventListener( 'pointerdown', onPointerDown );
+    }
+
 }
 
-let onMouseDown = function(e: MouseEvent)
+initSlider();
+
+function doMouseMove(event: MouseEvent)
 {
-    mouse.x = e.pageX;
-    mouse.y = e.pageY;
+    mouse.x = event.pageX
+    mouse.y = event.pageY
+    mouse.z = 1;
+
+    if(mouseDown) {
+        uniforms.mousePos.value.x = mouse.x/canvasWidth;
+        uniforms.mousePos.value.y = 1-mouse.y/canvasHeight;
+    }
+}
+
+function doMouseDown(event: MouseEvent)
+{
+    mouse.x = event.pageX;
+    mouse.y = event.pageY;
+    mouse.z = 1;
     mouseDown = true;
-    uniforms.mDown.value = true;
+    //uniforms.mDown.value = true;
+    if(!isInked){
+        isInked = true;
+        uniforms.ink.value = true;
+    }
 
-    uniforms.mousePos.value.x = mouseX/canvasWidth;
-    uniforms.mousePos.value.y = (1-mouseY)/canvasHeight;
+    uniforms.mousePos.value.x = mouse.x/canvasWidth;
+    uniforms.mousePos.value.y = 1-mouse.y/canvasHeight;
 }
 
-let onMouseUp = function(e: Event)
+function onMouseUp(event: Event)
 {
     mouseDown = false;
-    uniforms.mDown.value = false;
+    //uniforms.mDown.value = false;
 }
 
 function handleMouseMove(event: MouseEvent) {
@@ -195,11 +263,14 @@ function onWindowResize() {
     renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-window.onmousedown = onMouseDown;
-window.onmouseup = onMouseUp;
-window.onmousemove = onMouseMove;
+// window.onmousedown = onMouseDown;
+// window.onmouseup = onMouseUp;
+// window.onmousemove = onMouseMove;
 
-window.addEventListener("mousemove", handleMouseMove);
+window.addEventListener("mousemove", doMouseMove);
+window.addEventListener("mousedown",doMouseDown);
+window.addEventListener("mouseup",onMouseUp);
+// window.addEventListener("mousemove",handleMouseMove);
 
 let vertShaderText : string | null = "";
 if(vertShader) {
@@ -216,31 +287,6 @@ if(fragScreen) {
     fragScreenText = fragScreen.textContent;
 }
 
-// let fragStreamPlusText : string | null = "";
-// if(fragStreamPlus) {
-//     fragStreamPlusText = fragStreamPlus.textContent;
-// }
-
-// let fragStreamXText : string | null = "";
-// if(fragStreamX) {
-//     fragStreamXText = fragStreamX.textContent;
-// }
-
-// let fragVelDensUpdText : string | null = "";
-// if(fragVelDensUpd) {
-//     fragVelDensUpdText = fragVelDensUpd.textContent;
-// }
-
-// let fragRelaxPlusText : string | null = "";
-// if(fragRelaxPlus) {
-//     fragRelaxPlusText = fragRelaxPlus.textContent;
-// }
-
-// let fragRelaxXText : string | null = "";
-// if(fragRelaxX) {
-//     fragRelaxXText = fragRelaxX.textContent;
-// }
-
 let shaderMat = new THREE.ShaderMaterial({
     uniforms: uniforms,
     vertexShader: vertShaderText ? vertShaderText : "",
@@ -253,37 +299,6 @@ let shaderMat2 = new THREE.ShaderMaterial({
     fragmentShader: fragScreenText ? fragScreenText : "",
 });
 
-// let shaderStreamPlus = new THREE.ShaderMaterial({
-//     uniforms: uniforms,
-//     vertexShader: vertShaderText ? vertShaderText : "",
-//     fragmentShader: fragStreamPlusText ? fragStreamPlusText : "",
-// });
-
-// let shaderStreamX = new THREE.ShaderMaterial({
-//     uniforms: uniforms,
-//     vertexShader: vertShaderText ? vertShaderText : "",
-//     fragmentShader: fragStreamXText ? fragStreamXText : "",
-// });
-
-// let shaderVelDensUpd = new THREE.ShaderMaterial({
-//     uniforms: uniforms,
-//     vertexShader: vertShaderText ? vertShaderText : "",
-//     fragmentShader: fragVelDensUpdText ? fragVelDensUpdText : "",
-// });
-
-// let shaderRelaxPlus = new THREE.ShaderMaterial({
-//     uniforms: uniforms,
-//     vertexShader: vertShaderText ? vertShaderText : "",
-//     fragmentShader: fragRelaxPlusText ? fragRelaxPlusText : "",
-// });
-// let shaderRelaxX = new THREE.ShaderMaterial({
-//     uniforms: uniforms,
-//     vertexShader: vertShaderText ? vertShaderText : "",
-//     fragmentShader: fragRelaxXText ? fragRelaxXText : "",
-// });
-
-
-
 paperMat.map = bufferTexture.texture;
 paperMat.map.wrapS = THREE.RepeatWrapping;
 paperMat.map.wrapT = THREE.RepeatWrapping;
@@ -292,20 +307,8 @@ const paperObj = new THREE.Mesh(geometry, shaderMat2);
 
 const cube = new THREE.Mesh(geometry, shaderMat);
 
-// const streamPlusCube = new THREE.Mesh(geometry,shaderStreamPlus);
-// const streamXCube = new THREE.Mesh(geometry,shaderStreamX);
-// const velDensUpdCube = new THREE.Mesh(geometry,shaderVelDensUpd);
-// const relaxPlusCube = new THREE.Mesh(geometry,shaderRelaxPlus);
-// const relaxXCube = new THREE.Mesh(geometry,shaderRelaxX);
-
 bufferScene.add(cube)
 scene.add(paperObj);
-
-// StreamPlusScene.add(streamPlusCube);
-// StreamXScene.add(streamXCube);
-// VelDensScene.add(velDensUpdCube);
-// RelaxPlusScene.add(relaxPlusCube);
-// RelaxXScene.add(relaxXCube);
 
 const colorSet = { 
     red: new THREE.Vector4(1,0,0,1),
@@ -313,6 +316,18 @@ const colorSet = {
     blue: new THREE.Vector4(0,0,1,1),
 };
 const curColor = {Color: new THREE.Vector4(1,0,0,1)};
+
+const renderStages = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+];
+
+const curStage = {Stage: 6};
 
 const gui = new GUI();
 const colorFolder = gui.addFolder('Color');
@@ -322,6 +337,8 @@ const uniformFolder = gui.addFolder('Uniforms');
 uniformFolder.add(uniforms.omg,"value",0.5,1.5).name('Omega');
 uniformFolder.add(uniforms.rho0,"value",0.5,1.5).name('rho0');
 uniformFolder.add(uniforms.c,"value",0.5,1.5).name('c');
+uniformFolder.add(curStage,'Stage',renderStages);
+//uniformFolder.add(uniforms.reboundTexture.)
 
 
 let screenToggle = false;
@@ -333,32 +350,39 @@ function render() {
     uniforms.inkColor.value = curColor.Color;
     uniforms.iTime.value = (Date.now() - initTime)/1000;
     // Render onto our off-screen texture
-    uniforms.shaderStage.value = 0;
+    uniforms.shaderStage.value = 5;
+    renderer.setClearColor("rgb(0, 0, 0)");
+    if(mouseDown) {
+        uniforms.mDown.value = true;
+    }
     if(screenToggle){
         uniforms.stVecs.value = StVecs2.texture;
         uniforms.diagVecs.value = DiagVecs2.texture;
         uniforms.otherVecs.value = OtherVecs1.texture;
-        renderer.setClearColor("rgb(0, 0, 0)");
+        uniforms.reboundTexture.value = waterAndBoundary1.texture;
+        uniforms.pigmentTexture1.value = redLayer1.texture;
+        renderer.setRenderTarget(waterAndBoundary2);
+        renderer.render(scene, camera);
+        uniforms.shaderStage.value = 0;
         renderer.setRenderTarget(StVecs1);
         renderer.render(scene, camera);
         uniforms.shaderStage.value = 1;
-        renderer.setClearColor("rgb(0, 0, 0)");
         renderer.setRenderTarget(DiagVecs1);
         renderer.render(scene, camera);
         uniforms.shaderStage.value = 2;
-        renderer.setClearColor("rgb(0, 0, 0)");
         renderer.setRenderTarget(OtherVecs2);
         renderer.render(scene, camera);
         uniforms.stVecs.value = StVecs1.texture;
         uniforms.diagVecs.value = DiagVecs1.texture;
         uniforms.otherVecs.value = OtherVecs2.texture;
         uniforms.shaderStage.value = 3;
-        renderer.setClearColor("rgb(0, 0, 0)");
         renderer.setRenderTarget(StVecs2);
         renderer.render(scene, camera);
         uniforms.shaderStage.value = 4;
-        renderer.setClearColor("rgb(0, 0, 0)");
         renderer.setRenderTarget(DiagVecs2);
+        renderer.render(scene, camera);
+        uniforms.shaderStage.value = 6;
+        renderer.setRenderTarget(redLayer2);
         renderer.render(scene, camera);
         uniforms.stVecs.value = StVecs2.texture;
         uniforms.diagVecs.value = DiagVecs2.texture;
@@ -367,36 +391,47 @@ function render() {
         uniforms.stVecs.value = StVecs2.texture;
         uniforms.diagVecs.value = DiagVecs2.texture;
         uniforms.otherVecs.value = OtherVecs2.texture;
-        renderer.setClearColor("rgb(0, 0, 0)");
+        uniforms.reboundTexture.value = waterAndBoundary2.texture;
+        uniforms.pigmentTexture1.value = redLayer2.texture;
+        renderer.setRenderTarget(waterAndBoundary1);
+        renderer.render(scene, camera);
+        uniforms.shaderStage.value = 0;
         renderer.setRenderTarget(StVecs1);
         renderer.render(scene, camera);
         uniforms.shaderStage.value = 1;
-        renderer.setClearColor("rgb(0, 0, 0)");
         renderer.setRenderTarget(DiagVecs1);
         renderer.render(scene, camera);
         uniforms.shaderStage.value = 2;
-        renderer.setClearColor("rgb(0, 0, 0)");
         renderer.setRenderTarget(OtherVecs1);
         renderer.render(scene, camera);
         uniforms.stVecs.value = StVecs1.texture;
         uniforms.diagVecs.value = DiagVecs1.texture;
         uniforms.otherVecs.value = OtherVecs1.texture;
         uniforms.shaderStage.value = 3;
-        renderer.setClearColor("rgb(0, 0, 0)");
         renderer.setRenderTarget(StVecs2);
         renderer.render(scene, camera);
         uniforms.shaderStage.value = 4;
-        renderer.setClearColor("rgb(0, 0, 0)");
         renderer.setRenderTarget(DiagVecs2);
+        renderer.render(scene, camera);
+        uniforms.shaderStage.value = 6;
+        renderer.setRenderTarget(redLayer1);
         renderer.render(scene, camera);
         uniforms.stVecs.value = StVecs2.texture;
         uniforms.diagVecs.value = DiagVecs2.texture;
         uniforms.otherVecs.value = OtherVecs1.texture;
     }
-    uniforms.shaderStage.value = 5;
+    if(!mouseDown) {
+        uniforms.mDown.value = false;
+    }
+    uniforms.shaderStage.value = 7;
     screenToggle = !screenToggle;
+    
+    renderer.setScissor( sliderPos, 0, window.innerWidth, window.innerHeight );
     renderer.setClearColor("rgb(150, 150, 150)");
     renderer.setRenderTarget(null);
+    renderer.render( scene, camera );
+    renderer.setScissor( 0, 0, sliderPos, window.innerHeight );
+    uniforms.shaderStage.value = curStage.Stage;
     renderer.render( scene, camera );
     initTime = Date.now();
 }
